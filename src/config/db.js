e2751dce90d62780cf9a9b8d02d053b9eb595d30
Mateusz Suder <matisu999@gmail.com>
2022-12-05 22:55:22 +0100

    Use envs, add json parser

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,16 @@
 import mongoose from "mongoose";
 
-const url = `mongodb://mongo:${process.env.DB_PORT}/${process.env.NAME}`;
+const url = `mongodb://${process.env.DB_CONTAINER_NAME}:${process.env.DB_PORT}/${process.env.NAME}`;
 
 const connectDb = () => {
     try {
         mongoose.connect(url, {
-            auth: {
-                username: process.env.DB_USERNAME,
-                password: process.env.DB_PASSWORD
-            },
+            user: process.env.DB_USERNAME,
+            pass: process.env.DB_PASSWORD,
             authSource: "admin"
         }, (error) => {
             if(error) {
-                console.error(error)
+                console.error("error", error)
             } else {
                 console.log("connected");
             }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import app from "./server/router.js";
 import connectDb from "./config/db.js";
+import express from "express";
 
 (async () => {
     connectDb();
     const port = process.env.PORT;
     if(!port) throw new Error("No port specified");
+
+    app.use(express.json());
 
     app.listen(port, async () => {
         console.log(`Microservice ${process.env.NAME} running on port ${port}`);
