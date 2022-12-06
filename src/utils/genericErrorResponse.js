880fe011ddb5dc2e12d328515bbb67b23aa42bad
Mateusz Suder <matisu999@gmail.com>
2022-12-06 22:09:55 +0100

    Handle message in error handler

--- src/utils/genericErrorResponse.js
+++ src/utils/genericErrorResponse.js
@@ -1,10 +1,11 @@
 /**
  * Create http error response
  * @param {e.Response} response
- * @param {Array<string>} message
+ * @param {Array<string> | string} message
  * @param {number} status
  */
 export default function genericErrorResponse(response, message, status) {
+    if(!Array.isArray(message)) message = [message];
     return response.status(status).send({
         error: message
     });
