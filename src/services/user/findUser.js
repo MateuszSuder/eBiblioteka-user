96404dd2609f160f73c9d1b942436b3eef3a3323
Mateusz Suder <matisu999@gmail.com>
2022-12-07 18:57:57 +0100

    Add possibility to retrive password

--- src/services/user/findUser.js
+++ src/services/user/findUser.js
@@ -2,7 +2,7 @@ import UserSchema from "../../schemas/UserSchema.js";
 import mongooseErrorResponse from "../../utils/mongooseErrorResponse.js";
 
 export default async (req, res) => {
-    const { email, id } = req.query;
+    const { email, id, includePassword } = req.query;
     console.log(email, id);
 
     try {
@@ -16,9 +16,13 @@ export default async (req, res) => {
 
         // todo add get without query - from jwt
 
-        query.select({ password: 0, __v: 0 });
+        const select = { __v: 0 }
+        if(!includePassword) select.password = 0;
+
+        query.select(select);
 
         query.exec((err, user) => {
+            console.log(err);
             if(err) return mongooseErrorResponse(res, err);
 
             return res.status(200).send(user);
