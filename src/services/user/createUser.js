483838ab0e9812c2391bda017839efb500662ce8
Mateusz Suder <matisu999@gmail.com>
2022-12-07 16:13:32 +0100

    Send id on success

--- src/services/user/createUser.js
+++ src/services/user/createUser.js
@@ -2,13 +2,12 @@ import UserSchema from "../../schemas/UserSchema.js";
 import mongooseErrorResponse from "../../utils/mongooseErrorResponse.js";
 
 export default async (req, res) => {
-    console.log("create", req.body);
     const user = new UserSchema({ ...req.body });
     try {
         const { id } = await user.save();
+
+        res.status(201).send({ id });
     } catch (e) {
         return mongooseErrorResponse(res, e);
     }
-
-    res.send("hello");
 }
