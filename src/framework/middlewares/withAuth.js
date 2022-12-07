eb128de89cc10b55a9d9ce37a2e1c5e7f65b5fb3
Mateusz Suder <matisu999@gmail.com>
2022-12-07 21:15:43 +0100

    Add auth middleware

---
+++ src/framework/middlewares/withAuth.js
@@ -0,0 +1,43 @@
+import internalFetcher from "../../http/internalFetcher.js";
+import genericErrorResponse from "../../utils/genericErrorResponse.js";
+
+/**
+ * @typedef Role
+ * @type {'ADMIN' | 'LIBRARIAN' | 'USER'}
+ */
+
+/**
+ * Middleware to authorize api route
+ * @param {object} options
+ * @param {null | Role} [options.role=null] Role to be required to be possible to access resource
+ * @return {(function(e.Request, e.Response, e.NextFunction): void)|*}
+ */
+const withAuth = (options= {
+    role: null
+}) => {
+    return async (req, res, next) => {
+        const apiKey = req.header("X-API-KEY");
+        if(apiKey === process.env.API_KEY) {
+            return next();
+        } else {
+            try {
+                const { payload: user } = await internalFetcher("auth", "POST", "authorize", {
+                    headers: {
+                        cookies: req.cookies
+                    }
+                })
+
+                if(options?.role) {
+                    if(!user.role) return genericErrorResponse(res, null, 403);
+
+                    if(ROLES[user.role] < ROLES[options.role]) return genericErrorResponse(res, null, 403);
+                }
+
+            } catch (e) {
+                return genericErrorResponse(res, null, e.status);
+            }
+        }
+
+        next();
+    }
+}
