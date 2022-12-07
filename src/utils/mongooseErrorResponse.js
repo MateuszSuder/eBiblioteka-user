dbd9bf95eea08c06f68a74d36d4bddbdceff14d8
Mateusz Suder <matisu999@gmail.com>
2022-12-07 02:52:30 +0100

    Add conflict handler

--- src/utils/mongooseErrorResponse.js
+++ src/utils/mongooseErrorResponse.js
@@ -6,6 +6,9 @@
 import genericErrorResponse from "./genericErrorResponse.js";
 
 export default function mongooseErrorResponse(response, e) {
+    if(e.code && e.code === 11000) {
+        return genericErrorResponse(response, "Conflict", 409);
+    }
     const errors = [];
 
     for(const error in e.errors) {
